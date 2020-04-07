4770994def8acbef7d14745db3f93cb32cf44db8
CristianX <thecristianx@hotmail.com>
2020-04-06 21:01:49 -0500

    Implementado el método de carga de la función cargarImagenesFirebase en el servicio; poniendo texto en verde cuando está cargado en el carga.component.html

--- src/app/services/carga-imagenes.service.ts
+++ src/app/services/carga-imagenes.service.ts
@@ -18,7 +18,40 @@ export class CargaImagenesService {
 
 
   cargarImagenesFirebase( imagenes: FileItemModule[] ) {
-    console.log( imagenes );
+    // console.log( imagenes );
+    const storageRef = firebase.storage().ref();
+
+    for ( const item of imagenes ) {
+      item.estaSubiendo = true;
+      if ( item.progreso >= 100 ) {
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef.child(`${ this.CARPETA_iMAGENES }/${ item.nombreArchivo }`)
+      .put( item.archivo );
+
+      // Subir imagen, barra de progreso
+      uploadTask.on( firebase.storage.TaskEvent.STATE_CHANGED, ( snapshot: firebase.storage.UploadTaskSnapshot ) =>
+      item.progreso = ( snapshot.bytesTransferred / snapshot.totalBytes ) * 100,
+      ( error ) => console.error('Error al subir', error),
+      () => {
+        console.log('Imagen cargada correctamente');
+
+        // Obteniendo url de imagen
+        uploadTask.snapshot.ref.getDownloadURL()
+        .then( ( url ) => {
+          item.url = url;
+          item.estaSubiendo = false;
+
+          // Guardando imagen en la bdd
+          this.guardarImagen({
+            nombre: item.nombreArchivo,
+            url: item.url
+          });
+        } );
+      }
+      );
+    }
   }
 
 
