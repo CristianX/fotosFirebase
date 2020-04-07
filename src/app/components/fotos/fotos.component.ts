8a1c8e348b9dab16637c2633baba793a25e8a344
CristianX <thecristianx@hotmail.com>
2020-04-06 21:28:33 -0500

    Agregando la llamada de fotos de firebase al fotos component.ts y llamandolas desde fotos.component.html

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// Angular Fire
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Item { nombre: string; url: string; }
+
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
@@ -7,7 +13,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+
+  constructor( private afs: AngularFirestore ) {
+    // Se asigna el nombre del nodo, en este caso img
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
+   }
 
   ngOnInit(): void {
   }
